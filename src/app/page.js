467a90080323e10cfa9cc9b4f085d418e6314a37
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-08 16:18:31 +0500

    first-commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1>Hello kese ho</h1>;
+  return <h1 className=" bg-priamryDark ">Hello kese ho</h1>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        priamryDark: "#252422",
+        primaryLight: "#f4f4f4",
+        primaryPink: "#f9cdcd",
       },
     },
   },
