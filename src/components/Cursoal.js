f80a8089f7775cc78fd50e2bad476f8dfced9feb
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-12 09:49:13 +0500

    hero page updated

--- src/components/Cursoal.js
+++ src/components/Cursoal.js
@@ -1,12 +1,12 @@
 "use client";
 import React, { useEffect, useState, useRef } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 function Cursoal() {
   const [isDown, setIsDown] = useState(false);
   const [startX, setStartX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
   const ref = useRef();
-  console.log("Off set ", startX), console.log(scrollLeft);
 
   useEffect(() => {
     const handleMouseDown = (e) => {
@@ -47,20 +47,25 @@ function Cursoal() {
   }, [isDown, startX, scrollLeft]);
 
   return (
-    <div>
+    <div className="bg-priamryDark  font-FoMed h-screen w-screen">
+      <div className="flex px-[5.5rem]  text-primaryPink py-[3.5rem]  bg-blue-900  justify-between ">
+        <div className="flex font-semibold before:content-[''] before:w-[0.7rem] before:h-[0.7rem] before:rounded-[50%] before:mt-1  before:bg-primaryPink text-[1rem] ">
+          <h1 className="ml-[2.2rem] leading-tight">
+            (5)INTERNAL WORKS <br /> ©24 c/o BASIC/DEPT
+          </h1>
+          <h1 className=" ml-[3.8rem] leading-tight">
+            A COLLECTION OF INTERNAL PROJECTS AND INITIATIVES UNDER THE
+            <br />
+            BASIC/DEPT BRAND.
+          </h1>
+        </div>
+        <AiOutlineCloseCircle className="text-[3rem] text-primaryPink" />
+      </div>
       <div
         ref={ref}
-        className="flex bg-red-200 overflow-hidden text-[30rem] gap-[4rem]"
+        className="flex bg-red-200 pointer-events-none overflow-hidden text-[30rem] gap-[4rem]"
       >
-        <div>
-          <h1>Hello</h1>
-        </div>
-        <div>
-          <p>Cursoal</p>
-        </div>
-        <div>
-          <p>Cursoal</p>
-        </div>
+        <ol></ol>
       </div>
     </div>
   );
