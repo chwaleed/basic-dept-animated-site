3045d9c4ded36a85a2df8f78fde9fa886293ea8f
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-13 22:32:54 +0500

    animation making

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Hero from "@/components/Hero";
-import Cursoal from "@/components/cursoal";
+import Cursoal from "@/components/Cursoal";
 export default function Home() {
   return (
     <>
--- src/components/Cursoal.js
+++ src/components/Cursoal.js
@@ -1,14 +1,19 @@
 "use client";
 import React, { useEffect, useState, useRef } from "react";
 import { AiOutlineCloseCircle } from "react-icons/ai";
+import menu1 from "../assets/Icons/menu1.webp";
+import Image from "next/image";
+import { motion, AnimatePresence, animate } from "framer-motion";
 
 function Cursoal() {
   const [isDown, setIsDown] = useState(false);
   const [startX, setStartX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
-  const ref = useRef();
+  const [hover, setHover] = useState(false);
+  const ref = useRef(null);
 
   useEffect(() => {
+    if (!ref.current) return;
     const handleMouseDown = (e) => {
       setIsDown(true);
       setStartX(e.pageX - ref.current.offsetLeft);
@@ -46,10 +51,19 @@ function Cursoal() {
     };
   }, [isDown, startX, scrollLeft]);
 
+  console.log(hover);
+  const variants = {
+    animate: {
+      transition: {
+        duration: 0.75,
+      },
+    },
+  };
+
   return (
     <div className="bg-priamryDark  font-FoMed h-screen w-screen">
-      <div className="flex px-[5.5rem]  text-primaryPink py-[3.5rem]  bg-blue-900  justify-between ">
-        <div className="flex font-semibold before:content-[''] before:w-[0.7rem] before:h-[0.7rem] before:rounded-[50%] before:mt-1  before:bg-primaryPink text-[1rem] ">
+      <div className="flex px-[6.2rem]  text-primaryPink py-[3.5rem]  bg-blue-900  justify-between ">
+        <div className="flex  before:content-[''] before:w-[0.8rem] before:h-[0.8rem] before:rounded-[50%] before:mt-1  before:bg-primaryPink text-[1rem] ">
           <h1 className="ml-[2.2rem] leading-tight">
             (5)INTERNAL WORKS <br /> ©24 c/o BASIC/DEPT
           </h1>
@@ -59,13 +73,71 @@ function Cursoal() {
             BASIC/DEPT BRAND.
           </h1>
         </div>
-        <AiOutlineCloseCircle className="text-[3rem] text-primaryPink" />
+        <AiOutlineCloseCircle className="text-[3rem] text-white" />
       </div>
-      <div
-        ref={ref}
-        className="flex bg-red-200 pointer-events-none overflow-hidden text-[30rem] gap-[4rem]"
-      >
-        <ol></ol>
+      <div ref={ref} className="flex   gap-[4rem]">
+        <motion.div
+          whileHover={() => setHover(true)}
+          // onMouseEnter={() => setHover(true)}
+          // onMouseLeave={() => setHover(false)}
+          className=" bg-slate-600 mt-2 ml-9 before:content-[''] overflow-hidden before:absolute before:left-0 before:w-[2px] before:bg-primaryPink before:z-10 before:h-[29rem] flex w-[30rem] h-[27rem]  items-center relative  flex-col "
+        >
+          <div className="w-[28rem] bg-red-600  pointer-events-none relative h-[22rem]">
+            {/* <Image
+              sizes="100"
+              src={menu1}
+              fill={true}
+              alt="1st Menu Image"
+              style={{ objectFit: "cover" }}
+            /> */}
+          </div>
+
+          <motion.div
+            // whileHover={{ y: "-200%" }}
+            initial={{ y: 0 }}
+            animate={hover && { y: "-200%" }}
+            exit={!hover && { y: "200%" }}
+            // layout
+            // initial={{ y: 100, opacity: 0 }}
+            // animate={hover && { y: 0, opacity: 100 }}
+            // transition={{ duration: 0.5 }}
+            // exit={{
+            //   y: 100,
+            //   opacity: 0,
+            //   layout: {
+            //     y: 100,
+            //   },
+            // }}
+            className="relative py-[1rem] w-full "
+          >
+            <div>
+              <div className=" px-7 text-primaryPink bg-priamryDark flex justify-between text-[1.4rem] font-bold">
+                <h1>MOVIES</h1>
+                <h1>2019</h1>
+              </div>
+              <h1 className="text-primaryPink px-7 bg-priamryDark  font-bold text-[1rem]">
+                PRODCAST SERIES
+              </h1>
+            </div>
+            <AnimatePresence>
+              <motion.div
+                // layout
+                initial={{ y: 100, opacity: 0 }}
+                animate={hover && { y: 0, opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                // exit={{ y: 100, opacity: 0 }}
+                className="text-primaryPink absolute z-10   px-7  mt-[2rem]  flex flex-col gap-[2rem]"
+              >
+                <p>
+                  Our prodcast series garneriing 45,000+ listens per episode
+                  providing candid converstion around variouos industries
+                  related topics.
+                </p>
+                <h1 className=" underline">Explore Brandbeats</h1>
+              </motion.div>
+            </AnimatePresence>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
