4d75ee13b8134221d2481adbfa848711597be0f0
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-29 12:41:27 +0500

    Making Card For landig page

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      <Image />
+      <h1>patagonia</h1>
+      <p>An eCommerce experience driven by Patagonia’s brand mission</p>
+    </div>
+  );
+}
+
+export default Card;
