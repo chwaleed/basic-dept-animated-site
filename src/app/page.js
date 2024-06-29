7dcd7c8cb18ed90fc99d935d619e9e29b07acea6
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-29 13:08:20 +0500

    making card in progress

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 import Button from "@/components/Button";
+import Card from "@/components/Card";
 import Hero from "@/components/Hero";
 import MainPage from "@/components/pages/MainPage";
 import { useState } from "react";
@@ -7,7 +8,8 @@ export default function Home() {
   return (
     <>
       {/* <Button /> */}
-      <MainPage />
+      {/* <MainPage /> */}
+      <Card />
     </>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Card1 from "@/assets/Card1.webp";
+import Card2 from "@/assets/Card1.webp";
+import Image from "next/image";
 
 function Card() {
   return (
-    <div>
-      <Image />
-      <h1>patagonia</h1>
+    <div className="flex-1   w-full">
+      <div className=" w-[40rem]  overflow-hidden relative">
+        <Image
+          src={Card1}
+          className=" relative z-20   overflow-hidden bg-yellow-600  transition-all  scale-105 hover:scale-100"
+          objectFit="cover"
+          alt="Card Image"
+        />
+      </div>
+      <h1 className="    uppercase font-semibold  text-[2rem]">patagonia</h1>
       <p>An eCommerce experience driven by Patagonia’s brand mission</p>
     </div>
   );
