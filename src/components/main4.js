ef2dae7e7cb19e512fa2b6ad45faab8326bcd825
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-02 14:43:43 +0500

    cursoal formed nearly

--- src/components/main4.js
+++ src/components/main4.js
@@ -16,11 +16,21 @@ function Part4() {
       <Carousel className="overflow-x-visible mt-[8rem]">
         <CarouselContent className=" mr-[5rem]  ">
           <CarouselItem>
-            <div className=" w-[35rem]">
+            <div className=" w-[28rem]  bg-pink-100 text-priamryDark">
               <h1 className="text-priamryDark text-[3rem] font-semibold">
                 Google
               </h1>
-              <hr className="bg-priamryDark w-[1.5rem] ml-1 mt-3 h-[5px]" />
+              <hr className="bg-priamryDark w-[1.5rem] ml-1 mt-3 h-[3px]" />
+              <div className="mt-20">
+                <h2 className=" uppercase text-[2rem] font-semibold">Google</h2>
+                <p className="text-[1.2rem] font-semibold mt-7">
+                  Our embedded partnership with Google is as deep as it gets.
+                  We’re the lead creative agency for Google Store and provide
+                  strategy, design, and prototyping to other divisions. Learn
+                  more about our partnership{" "}
+                  <a className="underline cursor-pointer">here</a>.
+                </p>
+              </div>
             </div>
           </CarouselItem>
         </CarouselContent>
