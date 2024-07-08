da8c257035e20d3bec523ac12341d6acb51683bf
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-08 18:54:48 +0500

    sticky component learning

--- src/components/main5.js
+++ src/components/main5.js
@@ -8,16 +8,20 @@ function Part5() {
   const isInView = useInView(ref, { once: true, amount: 0.6 }); // 60% in view
 
   return (
-    <motion.div className=" bg-priamryDark relative h-[150vh]  mb-[50vh]">
-      <div className="flex px-[5%] relative  bg-red-50 items-center">
-        <h1 className="flex-1 text-[7rem] sticky   leading-[100%] text-primaryPink font-bold">
+    <motion.div className=" bg-priamryDark   relative  h-[150vh]  mb-[50vh]">
+      <div className="flex px-[5%]  sticky  bg-red-50 ">
+        <h1 className=" text-[7rem]   bg-black h-[30%]  leading-[100%] text-primaryPink font-bold">
           BASIC/DEPT® <br />
           HELPS <br />
           BRANDS ● CONNECT W/ <br />
           CULTURE
         </h1>
-        <Image src={Card1} className="flex-1 h-[90vh]" alt="Card Image" />
       </div>
+      <Image
+        src={Card1}
+        className="flex-1 bg-slate-500 h-[90vh]"
+        alt="Card Image"
+      />
     </motion.div>
   );
 }
