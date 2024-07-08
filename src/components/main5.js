509524085e1d8f12d3130748cecbad2103178ca4
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-08 18:26:22 +0500

    making part 5 of mainpage

--- src/components/main5.js
+++ src/components/main5.js
@@ -1,14 +1,22 @@
 import React, { useRef } from "react";
 import { motion, useInView } from "framer-motion";
+import Card1 from "@/assets/Card1.webp";
+import Image from "next/image";
 
 function Part5() {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true, amount: 0.6 }); // 60% in view
 
   return (
-    <motion.div className="">
-      <div>
-        <div>BASIC/DEPT® HELPS BRANDS ● CONNECT W/ CULTURE</div>
+    <motion.div className=" bg-priamryDark  mb-[50vh]">
+      <div className="flex px-[5%] bg-red-50 items-center">
+        <div className="flex-1 text-[7rem]   leading-[100%] text-primaryPink font-bold">
+          BASIC/DEPT® <br />
+          HELPS <br />
+          BRANDS ● CONNECT W/ <br />
+          CULTURE
+        </div>
+        <Image src={Card1} className="flex-1 h-[100vh]" alt="Card Image" />
       </div>
     </motion.div>
   );
