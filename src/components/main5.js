e8b3fb0ae481dc2544443f22eaff9cd0083254fb
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-08 18:32:31 +0500

    making heading sticky

--- src/components/main5.js
+++ src/components/main5.js
@@ -8,15 +8,15 @@ function Part5() {
   const isInView = useInView(ref, { once: true, amount: 0.6 }); // 60% in view
 
   return (
-    <motion.div className=" bg-priamryDark  mb-[50vh]">
-      <div className="flex px-[5%] bg-red-50 items-center">
-        <div className="flex-1 text-[7rem]   leading-[100%] text-primaryPink font-bold">
+    <motion.div className=" bg-priamryDark relative h-[150vh]  mb-[50vh]">
+      <div className="flex px-[5%] relative  bg-red-50 items-center">
+        <h1 className="flex-1 text-[7rem] sticky   leading-[100%] text-primaryPink font-bold">
           BASIC/DEPT® <br />
           HELPS <br />
           BRANDS ● CONNECT W/ <br />
           CULTURE
-        </div>
-        <Image src={Card1} className="flex-1 h-[100vh]" alt="Card Image" />
+        </h1>
+        <Image src={Card1} className="flex-1 h-[90vh]" alt="Card Image" />
       </div>
     </motion.div>
   );
