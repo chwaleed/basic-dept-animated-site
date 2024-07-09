1e1bf0e4527fcb62a736b870c2a7e4d01869279f
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-09 11:54:51 +0500

    modifing button for pink color

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,14 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 function Button({ text }) {
+  const [isPink, setIsPink] = useState(false);
   return (
-    <button className=" uppercase border-[2px] relative before:bg-priamryDark before:h-full before:w-full before:content-[''] before:absolute before:left-0 overflow-hidden before:z-[-10]  before:origin-bottom before:top-[100%] hover:before:top-0 hover:text-white   before:transition-all  before:duration-200 transition-all font-semibold border-priamryDark px-8 py-[5px] rounded-3xl ">
+    <button
+      className={` uppercase border-[2px] relative ${
+        isPink ? "before:bg-primaryPink" : "before:bg-priamryDark"
+      }  before:h-full before:w-full before:content-[''] before:absolute before:left-0 overflow-hidden before:z-[-10]  before:origin-bottom before:top-[100%] hover:before:top-0 hover:text-white   before:transition-all  before:duration-200 transition-all font-semibold border-priamryDark px-8 py-[5px] rounded-3xl `}
+    >
       {text}
     </button>
   );
--- src/components/main5.js
+++ src/components/main5.js
@@ -2,22 +2,29 @@ import React, { useRef } from "react";
 import { motion, useInView } from "framer-motion";
 import Card1 from "@/assets/Card1.webp";
 import Image from "next/image";
+import Button from "./Button";
 
 function Part5() {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true, amount: 0.6 }); // 60% in view
 
   return (
-    <motion.div className="    h-[300vh] mb-[50vh]">
-      <div className=" px-[5%]   mt-5 mx-auto relative  bg-red-50 h-[130vh] ">
-        <h1 className="text-[8rem] sticky top-[4rem] inline-flex  leading-[100%] text-primaryPink font-bold bg-black">
-          BASIC/DEPT® <br />
-          HELPS <br />
-          BRANDS ●<br />
-          CONNECT W/ <br />
-          CULTURE
-        </h1>
-        <div className=" w-[45%] ml-32  inline    absolute   bg-slate-500 h-[120vh]">
+    <motion.div className=" bg-priamryDark  h-[300vh] mb-[50vh]">
+      <div className=" px-[5%]    mx-auto relative   h-[120vh] ">
+        <div className=" sticky top-[7rem] inline-block">
+          <h1 className="text-[8rem] text-primaryPink  leading-[100%]  font-bold ">
+            BASIC/DEPT® <br />
+            HELPS <br />
+            BRANDS ●<br />
+            CONNECT W/ <br />
+            CULTURE
+          </h1>
+          <h1 className="text-primaryPink text-[1.3rem] mt-[1.6rem]">
+            ADWEEK <span className="font-bold">AGENCY SPOTLIGHT</span>
+          </h1>
+          <Button text={"About us"} />
+        </div>
+        <div className=" w-[45%] ml-32  inline    absolute    h-[120vh]">
           <Image
             src={Card1}
             alt="Card Image"
